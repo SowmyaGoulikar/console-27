0b4aa9f2dc96c159e010c4524c69835ef26e8291
SowmyaGoulikar <146078900+SowmyaGoulikar@users.noreply.github.com>
2023-09-27 17:32:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+function varTask(){
+    if(true){
+        var myname="Sowmya";
+        console.log(myname);
+        myname="Is from";
+        console.log(myname);
+        myname="Miryalaguda";
+        
+    }
+    console.log(myname);
+}
+varTask();
+
+function letMeGuess(){
+    if(true){
+        let Mlg="Which is famous for Rice Mills";
+        console.log(Mlg);
+        Mlg="And is developing more and more now a days";
+        console.log(Mlg);
+        Mlg="Miryalaguda has so many places for outing like";
+        console.log(Mlg);
+        Mlg="Parks, Restaraunts,bakeries amd Temples";
+        console.log(Mlg);
+        Mlg="It also have Shopping malls too";
+        console.log(Mlg);
+        Mlg="Here food delivery via zomato,swiggy is available";
+        console.log(Mlg);
+    }
+}
+letMeGuess();
+
+function changeop(){
+    document.getElementById("vartolet").src="vartoletdfrnce.jpeg";
+}
+
+function constNever(){
+    const ismlg=" This is all about Miryalaguda";
+    console.log(ismlg);
+
+}
+constNever();
+
+function changeop1(){
+    document.getElementById("lettoconst").src="lettoconstdfrnce.jpeg";
+}
+
+function display(){
+    if(true){
+        var oceans="pacific";
+        document.getElementById("sonu").textContent=oceans;
+    }
+}
+display();
+
+function show(){
+    if(true){
+        let ocean1="indian";
+        document.getElementById("alpa").textContent=ocean1;
+    }
+}
+show();
+
+function monitor(){
+    if(true){
+        const ocean2="atlantic";
+        document.getElementById("harsha").textContent=ocean2;
+    }
+}
+monitor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
